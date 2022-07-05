ce89feab3087c48c80c809a9aae25c53a3a1dcb4
john <johngavinroser@gmail.com>
2022-07-05 10:17:57 -0500

    Setup for dist

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-requirements = open("requirements.txt").readlines()
 
-setup(
-    name='HiddenRequest',
-    packages=find_packages("src"),  # include all packages under src
-    package_dir={'': 'src'},
-    version="0.1.1",
-    author='groseries',
-    description='An extension of tor/requests using ProtonVPN.',
-    install_requires=requirements
-)
+setup()
